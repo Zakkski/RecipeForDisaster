2def6fd0b160acd63bad1b4b8fed3cd8adf6c290
ZKwiecinski <zachary.kwiecinski@gmail.com>
2020-01-05 15:41:24 -0500

    add files to git ignore

--- Disaster.API/Models/Ingredient.cs
+++
@@ -1,9 +0,0 @@
-namespace Disaster.API.Models
-{
-    public class Ingredient
-    {
-        public int IngredientId { get; set; }
-        public string Name { get; set; }
-        public bool Count { get; set; }
-    }
-}
